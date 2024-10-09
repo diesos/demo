7277232c65e1db4bd6f768f93faa2a1fe13e1a88
Diesos <ozturk@live.Fr>
2024-10-09 15:25:31 +0200

    Added getter and setters

--- src/main/java/foreach/cda/Models/Cour.java
+++ src/main/java/foreach/cda/Models/Cour.java
@@ -8,4 +8,44 @@ public class Cour {
 	private Ue ue;
 	private Formateur formateur;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	public Ue getUe() {
+		return ue;
+	}
+
+	public void setUe(Ue ue) {
+		this.ue = ue;
+	}
+
+	public Formateur getFormateur() {
+		return formateur;
+	}
+
+	public void setFormateur(Formateur formateur) {
+		this.formateur = formateur;
+	}
+
 }
--- src/main/java/foreach/cda/Models/Ue.java
+++ src/main/java/foreach/cda/Models/Ue.java
@@ -3,4 +3,13 @@ package foreach.cda.Models;
 public class Ue {
 	private int id;
 	private String Libelle;
+
+	public String getLibelle() {
+		return Libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		Libelle = libelle;
+	}
+
 }
